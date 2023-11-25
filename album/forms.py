653df20731db0fc7ed6533d2330df6a4b097cdcf
m2ttri <matrixjke@gmail.com>
2023-11-25 14:24:39 +0300

    upd5

--- album/forms.py
+++ album/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Album, Image
+from django.forms import ClearableFileInput, FileField
 
 
 class AlbumForm(forms.ModelForm):
@@ -25,3 +26,26 @@ class AlbumEditForm(forms.ModelForm):
     class Meta:
         model = Album
         fields = ['title', 'status']
+
+
+class MultipleFileInput(ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('widget', MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial)
+                      for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+
+class MultipleImageForm(forms.Form):
+    images = MultipleFileField(widget=MultipleFileInput(attrs={'multiple': True}))
--- album/views.py
+++ album/views.py
@@ -2,22 +2,42 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from .models import Album
-from .forms import AlbumForm, AlbumEditForm, ImageForm
+from .models import Album, Image
+from .forms import AlbumForm, AlbumEditForm, MultipleImageForm
+
+
+@login_required()
+def create_album(request):
+    if request.method == 'POST':
+        form = AlbumForm(request.POST)
+        image_form = MultipleImageForm(request.POST,
+                                       request.FILES)
+        if form.is_valid() and image_form.is_valid():
+            album = form.save(commit=False)
+            album.author = request.user
+            album.save()
+            for image in request.FILES.getlist('images'):
+                Image.objects.create(image=image, album=album)
+            return redirect('album:album_detail', album.id)
+    else:
+        form = AlbumForm()
+        image_form = MultipleImageForm()
+    return render(request,
+                  'album/create.html',
+                  {'form': form, 'image_form': image_form})
 
 
 def album_detail(request, id):
     album = get_object_or_404(Album, id=id)
     if request.method == 'POST':
-        add_image_form = ImageForm(request.POST,
-                                   request.FILES)
+        add_image_form = MultipleImageForm(request.POST,
+                                           request.FILES)
         if add_image_form.is_valid():
-            images = add_image_form.save(commit=False)
-            images.album = album
-            images.save()
+            for image in request.FILES.getlist('images'):
+                Image.objects.create(image=image, album=album)
             return redirect('album:album_detail', album.id)
     else:
-        add_image_form = ImageForm()
+        add_image_form = MultipleImageForm()
     return render(request,
                   'album/detail.html',
                   {'album': album, 'add_image_form': add_image_form})
@@ -34,26 +54,26 @@ def album_list(request):
                   {'posts': posts})
 
 
-@login_required()
-def create_album(request):
-    if request.method == 'POST':
-        form = AlbumForm(request.POST)
-        image_form = ImageForm(request.POST,
-                               request.FILES)
-        if form.is_valid() and image_form.is_valid():
-            album = form.save(commit=False)
-            album.author = request.user
-            album.save()
-            images = image_form.save(commit=False)
-            images.album = album
-            images.save()
-            return redirect('album:album_detail', album.id)
-    else:
-        form = AlbumForm()
-        image_form = ImageForm()
-    return render(request,
-                  'album/create.html',
-                  {'form': form, 'images_form': image_form})
+# @login_required()
+# def create_album(request):
+#     if request.method == 'POST':
+#         form = AlbumForm(request.POST)
+#         image_form = ImageForm(request.POST,
+#                                request.FILES)
+#         if form.is_valid() and image_form.is_valid():
+#             album = form.save(commit=False)
+#             album.author = request.user
+#             album.save()
+#             images = image_form.save(commit=False)
+#             images.album = album
+#             images.save()
+#             return redirect('album:album_detail', album.id)
+#     else:
+#         form = AlbumForm()
+#         image_form = ImageForm()
+#     return render(request,
+#                   'album/create.html',
+#                   {'form': form, 'images_form': image_form})
 
 
 @login_required
