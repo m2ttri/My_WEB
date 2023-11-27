ca06bf8cb0f82d1161fcba7b1b9e1ab2738331f7
m2ttri <matrixjke@gmail.com>
2023-11-27 14:26:54 +0300

    upd8

--- album/urls.py
+++ album/urls.py
@@ -10,10 +10,9 @@ urlpatterns = [
     path('create/', views.create_album, name='album_create'),
     path('<int:id>/', views.album_detail, name='album_detail'),
     path('<int:id>/edit/', views.edit_album, name='album_edit'),
-    path('album/<int:pk>/delete/',
-         AlbumDeleteView.as_view(), name='album_delete'),
-    path('image/<int:pk>/delete/',
-         ImageDeleteView.as_view(), name='image_delete'),
-    path('image/<int:image_id>/download',
-         views.download_image, name='image_download'),
+    path('album/<int:pk>/delete/', AlbumDeleteView.as_view(), name='album_delete'),
+    path('image/<int:pk>/delete/', ImageDeleteView.as_view(), name='image_delete'),
+    path('image/<int:image_id>/download', views.download_image, name='image_download'),
+
+    path('image/<int:image_id>/', views.image_detail, name='image_details'),
 ]
